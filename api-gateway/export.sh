8f0cc10f86df04eb5858b40a18e1cd7cc22303e3
llavedure@realdecoy.com <llavedure@realdecoy.com>
2018-01-24 06:21:06 -0500

    Api gateway export

---
+++ api-gateway/export.sh
@@ -0,0 +1,10 @@
+aws apigateway get-rest-apis > apis.json
+rm -rf apis
+mkdir apis
+jq -r '.items[] | .id' apis.json | while read i; do
+	aws apigateway get-stages --rest-api-id $i > stages.json
+	jq -r '.item[] | .stageName' stages.json | while read z; do
+		aws apigateway get-export --rest-api-id $i --stage-name $z --export-type 'swagger' "apis/$z-$i.json"
+	done
+done
+#aws apigateway get-export --rest-api-id dev-my-tdd-service --stage-name  dev-my-tdd-service --export-type swagger
