b78d0b066bec841598d28b45ef3f6bf72fc9ecc1
llavedure@realdecoy.com <llavedure@realdecoy.com>
2018-01-24 04:38:55 -0500

    dos2unix fix

--- buckets/export.sh
+++ buckets/export.sh
@@ -1,9 +1,5 @@
-#!/usr/bin/env bash
-
-LAMBDA_BUCKET="sdpf-webapp"
-# change this ENV variable depending on the environment you want to deploy
-ENV="dev"
-STACK_NAME="aws-lambda-cf-environments-${ENV}"
-
-# now package the CloudFormation template, upload SAM artifacts to S3 and deploy it
-aws cloudformation package --template-file cfn.yml --s3-bucket ${LAMBDA_BUCKET} --output-template-file cfn.packaged.yml
+aws s3api list-buckets --query "Buckets[].Name" > buckets.json
+jq -r '.[]' buckets.json | while read i; do
+	mkdir $i
+	aws s3 sync s3://$i $i
+done
