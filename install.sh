9d1aefe2f131fc9c67f6da011155b06ce1e15ea6
llavedure@realdecoy.com <llavedure@realdecoy.com>
2018-01-24 10:38:52 -0500

    Added install to get all dependencies

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo chmod 777 -R .
+sudo apt-get install python-pip
+sudo pip install awscli
+sudo pip install --upgrade pip
+sudo apt-get install dos2unix
+sudo apt-get install jq
+sudo apt-get install python-boto
+sudo chmod 777 -R .
+find . -type f -print0 | xargs -0 dos2unix
+aws configure
